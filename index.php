750fa2eba89688dd8cbdab0956aace66685ace1d
Diogo Silva <diogosilvaoficial@gmail.com>
2017-11-29 20:02:37 -0200

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Weband - Site</title>
+
+    <!--Import Google Icon Font-->
+    <link rel="stylesheet "href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!-- Carousel -->
+    <link rel="stylesheet" href="includes/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="includes/css/owl.theme.default.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="includes/css/font-awesome.min.css" />
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="includes/css/style.css" />
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="includes/css/materialize.css" />
+    <link rel="stylesheet" href="includes/css/custom.css" />
+    <!-- jQuery Scrollpane -->
+    <link rel="stylesheet" href="includes/css/jquery.jscrollpane.css" />
+
+  </head>
+  <body>
+
+    <!-- TOPO -->
+
+    <header>
+      <div class="navbar-fixed">
+        <nav class="transparent">
+          <div class="nav-wrapper menu-background">
+            <div class="container">
+            <a href="#" class="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="#home">HOME</a></li>
+              <li><a href="#agenda">AGENDA</a></li>
+              <li><a href="#release">RELEASE</a></li>
+              <li><a href="#musicas">MÚSICAS</a></li>
+              <li><a href="#videos">VIDEOS</a></li>
+              <li><a href="#fotos">FOTOS</a></li>
+              <li><a href="#redes_sociais">REDES SOCIAIS</a></li>
+              <li><a href="#contato">CONTATO</a></li>
+            </ul>
+        </nav>
+        </div>
+</div>
+            <div id="mobile-demo" class="side-nav">
+              <ul>
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#agenda">AGENDA</a></li>
+                <li><a href="#release">RELEASE</a></li>
+                <li><a href="#musicas">MÚSICAS</a></li>
+                <li><a href="#videos">VIDEOS</a></li>
+                <li><a href="#fotos">FOTOS</a></li>
+                <li><a href="#redes_sociais">REDES SOCIAIS</a></li>
+                <li><a href="#contato">CONTATO</a></li>
+               <li><div class="divider"></div></li>
+             </ul>
+
+
+           </div>
+    </header>
+
+    <!-- SESSÃO DO SITE -->
+
+    <!-- HOME -->
+
+    <section>
+      <div id="home" class="section scrollspy parallax-container">
+        <div class="parallax">
+          <img src="https://s2.glbimg.com/bZJZ7zlFD2vbCuvH2w80wwvimak=/0x0:1600x1067/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/i/V/UJvHpyRByNf5DZ7Kyf1A/2.jpg">
+          </div>
+
+          <div class="row">
+            <div class="col s6 logomarca">
+              <h2>Sua Logo Aqui</h2>
+            </div>
+
+            <div class="col s6">
+              <ul class="social container">
+                <li><a href="#Facebook"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                <li><a href="#Facebook"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a></li>
+                <li><a href="#Facebook"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></li>
+                <li><a href="#Facebook"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+              </ul>
+            </div>
+          </div>
+
+          </div>
+      </div>
+
+
+
+      <!-- AGENDA -->
+      <div class="col s12">
+        <div id="agenda" class="section scrollspy">
+
+          <div class="row">
+              <h2 class="agenda-titulo">PRÓXIMOS SHOWS</h2>
+                <div class="col s2 seta-left">
+                  <a href="#" class="img-prev arrow"></a><i class="material-icons large white-text">keyboard_arrow_left</i>
+                </div>
+
+                <div class="col s8">
+                  <!-- Set up your HTML -->
+                  <div class="owl-carousel owl-theme agenda">
+
+                    <div class="item">
+                      <div class="dia">21</div><br />
+                      <div class="mes">MAI</div><br />
+                      <div class="cidade">São Paulo - SP</div><br />
+                      <div class="local">Boteco Floresta</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col s2 seta-right">
+                  <i class="material-icons large white-text">keyboard_arrow_right</i>
+                </div>
+            </div>
+
+        </div>
+      </div>
+
+      <!-- RELEASE -->
+        <div id="release" class="section scrollspy purple">
+          <h2 class="title">RELEASE</h2>
+
+
+          <!-- RELASE DESKTOP -->
+          <div class="row container hide-on-med-and-down">
+
+            <!-- Coluna Esquerda -->
+            <div class="col s5">
+
+            </div>
+
+            <!-- Coluna Direita -->
+            <div class="col s7">
+              <div class="scroll-pane white-text">
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+              </div>
+            </div>
+
+          </div>
+
+          <!-- RELEASE TABLET & MOBILE -->
+          <div class="row container hide-on-large-only">
+            <div class="col s12 container">
+              <div class="scroll-pane white-text">
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+
+
+      <!-- MÚSICAS -->
+
+          <div id="musicas" class="section scrollspy red">
+            <h2 class="title">MÚSICAS</h2>
+
+            <div class="row">
+
+              <div class="col s6">
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+              </div>
+
+              <div class="col s6">
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div id="videos" class="section scrollspy parallax-container">
+              <h2 class="title">VIDEOS</h2>
+              <div class="parallax"><img src="includes/img/home.jpg"></div>
+              <div class="container white-text">
+                <p>
+                  These examples show very basic functionality and exist so that I can test that any changes to jScrollPane work cross browser and don't cause any new problems. Note that these examples aren't meant to look pretty, they merely highlight different bits of functionaity which are available. If you want examples of jScrollPane looking good then check out the themes.
+                </p>
+              </div>
+          </div>
+
+          <div id="fotos" class="section scrollspy yellow">
+            <h2 class="title">FOTOS</h2>
+          </div>
+
+          <div id="redes_sociais" class="section scrollspy green">
+            <h2 class="title">REDES SOCIAIS</h2>
+          </div>
+
+          <div id="contato" class="section scrollspy white">
+            <div class="container">
+
+
+            <h2 class="title">CONTATO</h2>
+
+            <div class="row">
+              <form class="col s4">
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="Digite seu nome" id="nome" type="text" class="validate">
+                    <label for="nome">Nome</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="Digite seu e-mail" id="email" type="email" class="validate">
+                    <label for="email">E-mail</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="Digite o assunto" id="assunto" type="email" class="validate">
+                    <label for="assunto">Assunto</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="Digite sua mensagem" id="mensagem" type="text" class="validate">
+                    <label for="telefone">Mensagem</label>
+                  </div>
+                </div>
+              </form>
+            </div>
+            </div>
+          </div>
+        </div>
+
+
+    </section>
+
+
+
+    <footer class="page-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">Footer Content</h5>
+            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+        © 2017 Copyright Weband - Todos os Direitos Reservados
+        <a class="grey-text text-lighten-4 right" href="#!">DS WebSolutions</a>
+        </div>
+      </div>
+    </footer>
+
+
+    <!-- JAVASCRIPT -->
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <!-- Materialize JS -->
+    <script src="includes/js/materialize.js"></script>
+    <!-- Carousel -->
+    <script src="includes/js/owl.carousel.min.js"></script>
+    <!-- jQuery Scrollpane -->
+    <script src="includes/js/jquery.mousewheel.js"></script>
+    <script src="includes/js/jquery.jscrollpane.min.js"></script>
+
+
+    <script>
+      $( document ).ready(function(){
+
+        $('.parallax').parallax();
+
+        $(".button-collapse").sideNav();
+
+        $(document).ready(function(){
+          $('.scrollspy').scrollSpy({
+            scrollOffset: 65
+          });
+        });
+
+        $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          nav:false,
+          responsive:{
+            0:{
+              items:1
+            },
+            600:{
+              items:3
+            },
+            1000:{
+              items:4
+            }
+          }
+        })
+
+        $('.scroll-pane')
+        .jScrollPane({
+          showArrows: true,
+          verticalGutter: 10
+        })
+        .bind(
+            'mousewheel',
+            function(e)
+            {
+                e.preventDefault();
+            }
+        );
+
+      })
+    </script>
+
+  </body>
+</html>
